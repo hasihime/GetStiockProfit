a578498b52de99befea92eb66daf4f9ad798451a
hasihime <46011077+hasihime@users.noreply.github.com>
2020-12-13 20:25:11 +0900

    Make Test Controller

--- src/main/java/com/hasi/GetStockProfit/Presentation/GetStockProfitController.java
+++ src/main/java/com/hasi/GetStockProfit/Presentation/GetStockProfitController.java
@@ -1,4 +1,16 @@
 package com.hasi.GetStockProfit.Presentation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GetStockProfitController {
+
+
+
+    // TODO : 카카오톡 채널봇에서 국내주식인지 해외주식인지 받아서 들어온 요청에 따라 분기 제공
+
+
+
+    //
 }
