06ae9dd21c65b6d39b24239ecfb9231df22eaa5b
hasihime <46011077+hasihime@users.noreply.github.com>
2020-12-17 19:46:19 +0900

    change Controller function

--- src/main/java/com/hasi/GetStockProfit/Presentation/GetStockProfitController.java
+++ src/main/java/com/hasi/GetStockProfit/Presentation/GetStockProfitController.java
@@ -19,6 +19,9 @@ public class GetStockProfitController {
     // TODO : 카카오톡 채널봇에서 국내주식인지 해외주식인지 받아서 들어온 요청에 따라 분기 제공
 
 
+    // TODO : 국내 주식 코드를 받아서 정보를 조회해온다.
+
+    // TODO : 해외 주식 코드를 받아서 정보를 조회해온다.
 
     // TODO : TEST 컨트롤러- 결과를 JSON으로 출력해준다.
     @GetMapping(path = "/test/GetInterStockInfo", produces = "text/plain;charset=UTF-8")
